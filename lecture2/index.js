dd3cee983a1179cd59e1152111e1bde1e25b4ac7
UDAAKADAL <vaibhavchauhan12353@gmail.com>
2024-12-27 16:37:32 +0530

    added let variable

--- lecture2/index.js
+++ lecture2/index.js
@@ -1,13 +1,26 @@
+//using var datatype
 var age = 25;
+var age = 225; // redefination is possible
 
-if(true){
-    console.log(age);
-}
+// if(true){
+//     console.log(age);
+// }
 
-function add(){
-    var a=5
-    var b=6
-    console.log(a+b)
-    // console.log(age); it show error
+// function add(){
+//     var a=5
+//     var b=6
+//     console.log(a+b)
+//     // console.log(age); it show error
+// }
+// add()
+
+
+
+// now using let data type
+{
+    let a=10 
 }
-add()
+let b=1 
+// let b=5  re defination not possible
+// console.log(a)// variable a cannot acces outside the block
+console.log(b)
