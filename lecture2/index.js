caa66e73747ab1f6a9fbefedca36571719488528
UDAAKADAL <vaibhavchauhan12353@gmail.com>
2025-01-18 10:15:33 +0530

    update lec2

--- lecture2/index.js
+++ lecture2/index.js
@@ -1,6 +1,7 @@
-//using var datatype
-var age = 25;
-var age = 225; // redefination is possible
+// //**********************VARIABLE AND DATA TYPE****************************
+// //using var datatype
+// var age = 25;
+// var age = 225; // redefination is possible
 
 // if(true){
 //     console.log(age);
@@ -10,17 +11,49 @@ var age = 225; // redefination is possible
 //     var a=5
 //     var b=6
 //     console.log(a+b)
-//     // console.log(age); it show error
+//     // console.log(age); it show error becoz age is outside the add fun
 // }
 // add()
 
 
 
 // now using let data type
-{
-    let a=10 
-}
-let b=1 
-// let b=5  re defination not possible
-// console.log(a)// variable a cannot acces outside the block
+// {
+//     let a=10 
+// }
+// let b=1 
+// // let b=5  re defination not possible
+// // console.log(a)// variable a is not accesible
+// console.log(b);
+
+// let a = 5;
+// a="vaibhav";
+// a=2;
+// a="$$$$";
+// a=1.2323;
+// console.log(a) //reassignment are possible
+
+
+
+///now using const
+
+const a = 1;
+// a=4;
+console.log(a) 
+const b=10;
+// const b="vaibav";//re assignment and re definaation not possible 
 console.log(b)
+
+
+//*************Data Type *******************
+
+// 1> primitive 
+//     Number
+//     String
+//     null
+//     Boolean
+//     undefined
+//     BigInt
+//     Symbol
+2> non primitive
+
