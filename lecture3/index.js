dbdb34cbdfe4d29c976623438dacc1c586e54d22
UDAAKADAL <vaibhavchauhan12353@gmail.com>
2025-01-22 21:17:42 +0530

    new

---
+++ lecture3/index.js
@@ -0,0 +1,23 @@
+// console.log("hii")
+
+// ARITHMETIC OPERATION 
+let a = 4;
+let b = 7;
+
+console.log("a + b = ",a + b);//add
+console.log("a - b = ",a - b);//sub
+console.log("a * b = ",a * b);//multiply
+console.log("a / b = ",a / b);//divide 
+console.log("a % b = ",a % b); // modulo (reminder)
+console.log("a ** b = ",a ** b); //power
+
+
+// UNARY
+
+//pre increment
+console.log("pre increment =",++a);
+console.log("pre increment =",--a);
+
+//post increment
+console.log("post increment =",a++);
+console.log("post increment =",a--);
